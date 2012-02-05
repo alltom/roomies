62fe300e99eba5d663a39db7a7dab0672553c77a
Erek Speed <melink14@gmail.com>
2012-02-05 13:42:24 -0500

    Added buy and sell of chore_instance

--- app/models/chore_instance.rb
+++ app/models/chore_instance.rb
@@ -10,4 +10,28 @@ class ChoreInstance < ActiveRecord::Base
       o.offerer = resident
     end
   end
+
+  def sell price
+    self.price = price
+  end
+
+  def buy resident
+  
+    expense = Expense.create do |e|
+      e.payer = resident
+      e.amount = self.price
+    end
+
+    Expensed.create do |e|
+      e.expense = expense
+      e.resident = chore_instance.resident
+      e.amount = price
+    end
+
+    self.price = 0
+    chore_instance.resident = self.resident
+    Offer.delete(chore_instance.offers)
+
+  end
+
 end
