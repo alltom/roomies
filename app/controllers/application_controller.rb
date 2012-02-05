4d19fb8a7048f84d3935083b2b0d78439a96b733
Tom Lieber <tom@alltom.com>
2012-02-05 07:13:58 -0500

    moved current_resident to where it is useful

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  helper_method :current_resident
+  
+  def current_resident
+    current_user.residents.first # XXX
+  end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,6 @@
 class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
-  def current_resident
-    current_user.residents.first # XXX
-  end
-
   def index
       if not current_user
         render "login"
