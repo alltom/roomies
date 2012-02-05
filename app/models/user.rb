056eea85ad386b7ed226ea3d3200d955420939f6
Tom Lieber <tom@alltom.com>
2012-02-05 03:07:32 -0500

    lots of admin upgrades

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validate do
+    errors.add(:base, "Must belong to a residence") unless residents.length > 0
+  end
+  
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   
