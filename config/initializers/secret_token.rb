80442df7f72cec5a26b0dc3140c3a301bc4838df
Tom Lieber <tom@alltom.com>
2012-02-05 00:11:04 -0500

    imported rails skeleton project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roomies::Application.config.secret_token = '7c094f5f2ddb2b9fcde02cc8c3769837649425c0a5a09c72ff83d142106ac52f59de2f53cda13f9633bff492d48734379edd6f90f4906d46aa8638e81e404e22'
