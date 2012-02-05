5d3ebaa0596e03a80fabec12accf80c9beaeab11
Tom Lieber <tom@alltom.com>
2012-02-05 14:30:17 -0500

    try this for heroku

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module Roomies
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+    
+    # devise needs this on Heroku
+    config.assets.initialize_on_precompile = false
 
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
