758b64f0ab8a37615fe8c45563b09580f49d58b9
Erek Speed <melink14@gmail.com>
2012-02-05 13:36:02 -0500

    created offer and accept

--- app/models/chore_instance.rb
+++ app/models/chore_instance.rb
@@ -1,4 +1,13 @@
 class ChoreInstance < ActiveRecord::Base
   belongs_to :chore
   belongs_to :resident
+  has_many :offers
+
+  def offer resident, price
+    Offer.create do |o|
+      o.price = price
+      o.chore_instance = self
+      o.offerer = resident
+    end
+  end
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,2 +1,19 @@
 class Offer < ActiveRecord::Base
+belongs_to :chore_instance
+
+  def accept!
+    expense = Expense.create do |e|
+      e.payer = offerer
+      e.amount = price
+    end
+    Expensed.create do |e|
+      e.expense = expense
+      e.resident = chore_instance.resident
+      e.amount = price
+    end
+
+    chore_instance.resident = self.resident
+    Offer.delete(chore_instance.offers)
+  end
+
 end
