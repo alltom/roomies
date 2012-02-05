df54ebd796b3f24eb91547727e4436e47f84ca5b
Erek Speed <melink14@gmail.com>
2012-02-05 12:01:15 -0500

    Migrated Chore stuff

--- db/migrate/20120205162949_create_chore_instances.rb
+++ db/migrate/20120205162949_create_chore_instances.rb
@@ -1,10 +1,27 @@
 class CreateChoreInstances < ActiveRecord::Migration
   def change
-    create_table :chore_instances do |t|
+    create_table :chore do |t|
+      t.string :name
+      t.integer :interval
+      t.integer :lead_time
+      t.datetime :start_date
+      t.references :household
+      t.timestamps
+    end
+
+    create_table :offer do |t|
       t.decimal :price, :precision => 8, :scale => 2
+      t.references :chore_instance
+      t.references :offerer
+      t.timestamps
+    end
+
+    create_table :chore_instances do |t|
       t.datetime :start
       t.datetime :end
-
+      t.references :chore
+      t.references :resident
+      t.decimal :price, :precision => 8, :scale => 2
       t.timestamps
     end
   end
