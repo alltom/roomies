8f2bb232b833068ab6ca4c21fce401d691485296
Tom Lieber <tom@alltom.com>
2012-02-05 00:43:47 -0500

    regenerated project with ruby 1.9.2

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Roomies::Application.config.secret_token = '7c094f5f2ddb2b9fcde02cc8c3769837649425c0a5a09c72ff83d142106ac52f59de2f53cda13f9633bff492d48734379edd6f90f4906d46aa8638e81e404e22'
+Roomies::Application.config.secret_token = 'ac53c77d4fcffb8e635cb502d4cc0c91febd9c82caee4abd11bdc1b2003215d06b03c64c38fe683813df647b7c10d566c9d45750780e82526e5f42911581138a'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Roomies::Application.config.session_store :cookie_store, :key => '_roomies_session'
+Roomies::Application.config.session_store :cookie_store, key: '_roomies_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
