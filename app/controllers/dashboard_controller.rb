220b8174746d34b02b0fd13866677cd1945ddb41
Erek Speed <espeed@popple-mac.local>
2012-02-05 03:04:58 -0500

    Created money owed display untested

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,17 @@
 class DashboardController < ApplicationController
   before_filter :authenticate_user!
   
-  def index
+  def current_resident
+    current_user.residents.first # XXX
   end
+
+  def index
+      if not current_user
+        render "login"
+      else
+        @residents = current_resident.household.residents
+        @curRes = current_resident
+      end
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def current_resident
-    current_user.residents.first # XXX
-  end
 end
